264b0d178e38f4a88d0cb58479f6b953033429a2
MOURRAADOUMMOURRA <mourraadoummourra@gmail.com>
2024-12-21 17:39:16 +0100

    affichage de l'ecran terminer

--- script.js
+++ script.js
@@ -1,11 +1,37 @@
 const buttons = document.getElementsByTagName("button")
 const song=document.getElementById("song")
+const numbers=document.getElementsByClassName("number")
+const operators=document.getElementsByClassName("operator")
+const screen=document.getElementById("screen")
+let lastIsOperators=false
+let result=""
 for(i=0;i<buttons.length;i++){
     buttons[i].addEventListener("click",playSong)
 
 }
+for(i=0;i<numbers.length;i++){
+    numbers[i].addEventListener("click",getNumber)  
+}
+for(i=0;i<operators.length;i++){
+    operators[i].addEventListener("click",getOperators)  
+}
 function playSong(){
     song.play()
     
+    
 }
-console.log("song")
+function getNumber(event){
+    result= result+event.target.innerText
+    screen.innerText=result
+    
+    
+
+}
+function getOperators(event){
+    if(lastIsOperators==false){
+        result= result+event.target.innerText
+        screen.innerText=result
+        lastIsOperators=true
+    }
+}
+console.log(screen)
