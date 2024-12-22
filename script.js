2e7b2987d37da4901c90413c80ccb96957772594
MOURRAADOUMMOURRA <mourraadoummourra@gmail.com>
2024-12-22 18:33:46 +0100

    button de factoriel et aroudi termine

--- script.js
+++ script.js
@@ -8,6 +8,10 @@ const ac=document.getElementById("ac")
 const percentage=document.getElementById("percentage")
 const negative=document.getElementById("negative")
 const point=document.getElementById("point")
+const factoriel=document.getElementById("factoriel")
+const round=document.getElementById("round")
+const sin=document.getElementById("sin")
+const cos=document.getElementById("cos")
 let lastIsOperators=false
 let result=""
 for(i=0;i<buttons.length;i++){
@@ -74,3 +78,37 @@ point.addEventListener("click",()=>{
     result=result+"."
     screen.innerText=result
 })
+factoriel.addEventListener("click",()=>{
+    result=fact(eval(screen.innerText))
+    screen.innerText=result
+})
+
+function fact(n){
+    if (n==0){
+        return 1;
+     } else{
+          return  n*fact(n-1)
+        }
+}
+round.addEventListener("click",()=>{
+    result=Math.round(eval(screen.innerText))
+    screen.innerText=result
+})
+sin.addEventListener("click",()=>{
+    result=sinAproc(eval(screen.innerText)).toFixed(2)
+    console.log(result);
+    
+    screen.innerText=result
+})
+
+function sinAproc(x){
+    let resultSin=0;
+    let sign=1;
+    for(i=1;i<12;i+=2){
+        resultSin=resultSin+(sign*(Math.pow(x,i)/fact(i)));
+        console.log(resultSin);
+        
+        sign=sign*(-1);
+    }
+    return resultSin;
+}
