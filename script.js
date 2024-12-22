c043ce3ff9490f710271ff58569e74e39d328a93
MOURRAADOUMMOURRA <mourraadoummourra@gmail.com>
2024-12-22 17:18:02 +0100

    fin du calcatrice

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ const song=document.getElementById("song")
 const numbers=document.getElementsByClassName("number")
 const operators=document.getElementsByClassName("operator")
 const screen=document.getElementById("screen")
+const equal= document.getElementById("equal")
+const ac=document.getElementById("ac")
+const percentage=document.getElementById("percentage")
+const negative=document.getElementById("negative")
+const point=document.getElementById("point")
 let lastIsOperators=false
 let result=""
 for(i=0;i<buttons.length;i++){
@@ -20,18 +25,52 @@ function playSong(){
     
     
 }
+// voici la fonction qu'on execute sur un nombre
 function getNumber(event){
     result= result+event.target.innerText
     screen.innerText=result
+    lastIsOperators=false
     
     
 
 }
+// voici la fonction qu'on execute sur un operators
 function getOperators(event){
     if(lastIsOperators==false){
-        result= result+event.target.innerText
+        if(event.target.innerText=="X")
+            result=result+"*"
+        else
+            result= result+event.target.innerText
         screen.innerText=result
         lastIsOperators=true
     }
 }
-console.log(screen)
+equal.addEventListener("click",()=>{
+    screen.innerText=result=eval(result)
+})
+ac.addEventListener("click",()=>{
+    screen.innerText="0"
+    result=""
+})
+percentage.addEventListener("click",()=>{
+    result=result/100+"*"
+    screen.innerText=result
+    lastIsOperators=true
+})
+negative.addEventListener("click",()=>{
+    if(screen.innerText>0){
+        result="-"+ result
+    }else{
+        if(screen.innerText<0){
+            result=result.slice(1)
+        }else{
+            result="-"
+            lastIsOperator=true
+        }
+    }
+    screen.innerText=result
+})
+point.addEventListener("click",()=>{
+    result=result+"."
+    screen.innerText=result
+})
